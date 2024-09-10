688313ef6b1fe40bf95106120889ea59f5863155
AndrewOshawott <andrewoshawott@gmail.com>
2024-09-09 21:27:26 -0700

    Initial Commit :)

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-#import numpy as np
 import matplotlib.pyplot as plt
 import pandas
-from IPython.display import display
 
-exoplanet_rawdata = pandas.read_csv('exoplanet_rawdata.csv')
-df = exoplanet_rawdata[['Period', 'Rp/R*']]
-display(df)
+exoplanet_rawdata = pandas.read_csv('../../Downloads/exoplanet_rawdata.csv')
+df = exoplanet_rawdata[['KIC', 'KOI', 'Kepler', 'Period', 'Rp/R*', 'Status']]
+df = df[(df['Period']>0) & (df['Rp/R*']<15) & df['Status'].astype(str).str.startswith(' P')]
+print(df)
 
+df.to_csv('exoplanet_filtereddata.csv')
 
 plot = df.plot.scatter(x='Period', y='Rp/R*', c='blue', s=2)
-plt.xlim(left=0, right=60)
 plt.xlabel('Period (days)')
 plt.ylabel('Radius of planet (Earth radii)')
-plt.title('Exoplanet data: Period v Radius (Short periods only)')
+plt.title('Exoplanet data: Period v Radius (Filtered)')
 plt.show()
