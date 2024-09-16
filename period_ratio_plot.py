bcd5d63ba8cb736f208a9895deb4effa69ea7fdd
AndrewOshawott <andrewoshawott@gmail.com>
2024-09-15 17:11:32 -0700

    Week 3: Period Ratios (Updated Heatmap)

--- period_ratio_plot.py
+++ period_ratio_plot.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import pandas
+from matplotlib.colors import LogNorm
 
 exoplanet_multiplanetsystem_ratios = pandas.read_csv('exoplanet_multiplanetsystem_ratios.csv')
 df = exoplanet_multiplanetsystem_ratios[['KOI_inner','Period_inner','KOI_outer','Period_outer','Period Ratios']]
 print(df)
 
-plot = df.plot.scatter(x='Period_inner', y='Period_outer', c='Period Ratios', s=1, colormap='viridis')
+plot = df.plot.scatter(x='Period_inner', y='Period_outer', c='Period Ratios', s=1, colormap='viridis', norm=LogNorm(),colorbar=True)
 plt.xlabel('Period of inner planet in pair (days)')
 plt.ylabel('Period of outer planet in pair (days)')
 plt.xscale('log')
