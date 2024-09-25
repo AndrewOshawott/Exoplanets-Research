f01c7dc56cf78a721d56f8cc6a78c3781391e13d
AndrewOshawott <andrewoshawott@gmail.com>
2024-09-24 22:14:20 -0700

    idk but these were different lol

--- period_ratio_plot.py
+++ period_ratio_plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas
 from matplotlib.colors import LogNorm
+from matplotlib.ticker import LogFormatter
 
 exoplanet_multiplanetsystem_ratios = pandas.read_csv('exoplanet_multiplanetsystem_ratios.csv')
 df = exoplanet_multiplanetsystem_ratios[['KOI_inner','Period_inner','KOI_outer','Period_outer','Period Ratios']]
@@ -11,5 +12,5 @@ plt.xlabel('Period of inner planet in pair (days)')
 plt.ylabel('Period of outer planet in pair (days)')
 plt.xscale('log')
 plt.yscale('log')
-plt.title('Exoplanet data: Period of inner planet in pair vs Period of outer planet in pair')
+plt.title('Exoplanet data: Periods of inner planet vs outer planet in pair')
 plt.show()
