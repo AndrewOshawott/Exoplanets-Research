b612341dddc2ca6d29bbf8586a805fd295fd4a8b
AndrewOshawott <andrewoshawott@gmail.com>
2024-09-29 20:52:28 -0700

    Histogram stuff

--- histogram.py
+++ histogram.py
@@ -3,18 +3,14 @@ import pandas
 
 exoplanet_multiplanetsystem_ratios = pandas.read_csv('exoplanet_multiplanetsystem_ratios.csv')
 df = exoplanet_multiplanetsystem_ratios[['KOI_inner','Period_inner','KOI_outer','Period_outer','Period Ratios']]
+df = df[(df['Period Ratios']<4.0)]
 print(df)
 
-histogram = df.plot.hist(column='Period Ratios', bins=96)
-plt.xlabel('Period Ratios')
-plt.xlim(1,4.1)
-plt.xscale('log')
-
-plt.title('Exoplanet data: Histogram of period ratios')
-plt.show()
-
-histogram2 = df.plot.hist(column='Period Ratios', bins=96)
-plt.xlabel('Period Ratios')
-plt.xlim(1,4.1)
+histogram = df.plot.hist(column='Period Ratios', bins=96, color='blue')
+plt.xlabel('Period Ratios $(P_{outer}/P_{inner})$')
+plt.ylabel('$N$')
+plt.xlim(1,4.0)
+plt.ylim(0,41)
+plt.legend().remove()
 plt.title('Exoplanet data: Histogram of period ratios')
 plt.show()
